ac327eee9aab7d3d3f5ad5b4b2b198158866db71
David Georg Reichelt <davidgeorg_reichelt@dagere.de>
2022-02-08 17:44:50 +0100

    Fix RegularPeassdataReader and add test

--- precision-analysis/src/main/java/de/precision/analysis/repetitions/RegularPeassdataReader.java
+++ precision-analysis/src/main/java/de/precision/analysis/repetitions/RegularPeassdataReader.java
@@ -3,6 +3,7 @@ package de.precision.analysis.repetitions;
 import java.io.File;
 import java.io.FileFilter;
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 
 import javax.xml.bind.JAXBException;
@@ -17,37 +18,60 @@ import de.dagere.kopeme.generated.Result;
 public class RegularPeassdataReader {
    private final Map<String, Testcases> testcasesV1 = new LinkedHashMap<>();
    private final Map<String, Testcases> testcasesV2 = new LinkedHashMap<>();
-   
+
    public void read(final File versionFile, final File testclazzFile) throws JAXBException {
       for (File subversionFile : versionFile.listFiles()) {
-         Testcases current = new Testcases();
+//         Testcases current = new Testcases();
          for (File vmRun : subversionFile.listFiles((FileFilter) new WildcardFileFilter("*xml"))) {
+            Testcases current = getTestcases(versionFile, testclazzFile, subversionFile, vmRun);
+            
+            
             Kopemedata data = XMLDataLoader.loadData(vmRun);
             Testcases internalData = data.getTestcases();
-            current.getTestcase().add(internalData.getTestcase().get(0));
-         }
-         if (subversionFile.getName().equals(versionFile.getName())) {
-            testcasesV1.put(testclazzFile.getName(), current);
-         } else {
-            testcasesV2.put(testclazzFile.getName(), current);
+            if (current.getTestcase().size() > 0) {
+               List<Result> addableResults = internalData.getTestcase().get(0).getDatacollector().get(0).getResult();
+               current.getTestcase().get(0).getDatacollector().get(0).getResult().addAll(addableResults);
+            } else {
+               current.getTestcase().add(internalData.getTestcase().get(0));
+            }
          }
       }
    }
-   
+
+   private Testcases getTestcases(final File versionFile, final File testclazzFile, File subversionFile, File vmRun) {
+      String testMethodName = vmRun.getName().substring(0, vmRun.getName().indexOf("_"));
+      Testcases current;
+      String testcaseName = testclazzFile.getName() + "#" + testMethodName;
+      if (subversionFile.getName().equals(versionFile.getName())) {
+         current = testcasesV1.get(testcaseName);
+         if (current == null) {
+            current = new Testcases();
+            testcasesV1.put(testcaseName, current);
+         }
+      } else {
+         current = testcasesV2.get(testcaseName);
+         if (current == null) {
+            current = new Testcases();
+            testcasesV2.put(testcaseName, current);
+         }
+      }
+      return current;
+   }
+
    public Map<String, Testcases> getTestcasesV1() {
       return testcasesV1;
    }
-   
+
    public Map<String, Testcases> getTestcasesV2() {
       return testcasesV2;
    }
-   
+
    public int getRepetitions() {
       Result exampleResult = testcasesV1.values().iterator().next().getTestcase().get(0).getDatacollector().get(0).getResult().get(0);
       int repetitions = (int) exampleResult.getRepetitions();
       return repetitions;
    }
-   
+
    public int getIterations() {
       Result exampleResult = testcasesV1.values().iterator().next().getTestcase().get(0).getDatacollector().get(0).getResult().get(0);
       int iterations = (int) exampleResult.getIterations();
---
+++ precision-analysis/src/test/java/de/precision/analysis/repetitions/TestRegularPeassdataReader.java
@@ -0,0 +1,37 @@
+package de.precision.analysis.repetitions;
+
+import java.io.File;
+
+import javax.xml.bind.JAXBException;
+
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.collection.IsIterableWithSize;
+import org.junit.jupiter.api.Test;
+
+import de.dagere.kopeme.generated.Kopemedata.Testcases;
+
+public class TestRegularPeassdataReader {
+
+   @Test
+   public void testReading() throws JAXBException {
+      
+      File exampleData = new File("src/test/resources/measurementsFull_example");
+      
+      RegularPeassdataReader regularPeassdataReader = new RegularPeassdataReader();
+      File testcaseFolder = new File(exampleData, "measurements/de.test.CalleeTest");
+      File versionFolder = new File(testcaseFolder, "a23e385264c31def8dcda86c3cf64faa698c62d8");
+      regularPeassdataReader.read(versionFolder, testcaseFolder);
+      
+      Testcases testcasesV1 = regularPeassdataReader.getTestcasesV1().get("de.test.CalleeTest#onlyCallMethod1");
+      MatcherAssert.assertThat(testcasesV1.getTestcase().get(0).getDatacollector().get(0).getResult(), IsIterableWithSize.iterableWithSize(3));
+      
+      Testcases testcasesV1_onlyCallmethod2 = regularPeassdataReader.getTestcasesV1().get("de.test.CalleeTest#onlyCallMethod2");
+      MatcherAssert.assertThat(testcasesV1_onlyCallmethod2.getTestcase().get(0).getDatacollector().get(0).getResult(), IsIterableWithSize.iterableWithSize(3));
+      
+      Testcases testcasesV2 = regularPeassdataReader.getTestcasesV2().get("de.test.CalleeTest#onlyCallMethod2");
+      MatcherAssert.assertThat(testcasesV2.getTestcase().get(0).getDatacollector().get(0).getResult(), IsIterableWithSize.iterableWithSize(3));
+      
+      Testcases testcasesV2_onlyCallmethod2 = regularPeassdataReader.getTestcasesV2().get("de.test.CalleeTest#onlyCallMethod2");
+      MatcherAssert.assertThat(testcasesV2_onlyCallmethod2.getTestcase().get(0).getDatacollector().get(0).getResult(), IsIterableWithSize.iterableWithSize(3));
+   }
+}
