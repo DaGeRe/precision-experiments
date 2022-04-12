6ca58490bd847276833f914c0490e371d9333b0f
David Georg Reichelt <davidgeorg_reichelt@dagere.de>
2022-04-12 02:51:05 +0200

    Also consider integer max and min

--- precision-analysis/scripts/metadata_analysis/getCalls.sh
+++ precision-analysis/scripts/metadata_analysis/getCalls.sh
@@ -39,13 +39,13 @@ function getProjectCalls {
 						        | egrep -v "[ ]*\([0-9]*\)[ ]*$" \
 						        | grep -v "#get" | grep -v "#is" | grep -v "#set" | grep -v ".get" | grep -v ".is" | grep -v ".set" | grep -v '#access\$' \
 						        | grep -v "super(" | grep -v "@Test" | grep -v "@PerformanceTest" | grep -v "@de.dagere.kopeme.annotations.PerformanceTest" | grep -v "continue" \
-						        | egrep -v "^[ ]*int [a-zA-Z_0-9]*;" | egrep -v "^[ ]*int [a-zA-Z_0-9]*[ ]*=[ ]*[-0-9]+;" | egrep -v "^[ ]*final int [a-zA-Z_0-9]*[ ]*=[ ]*[-0-9]+;" \
+						        | egrep -v "^[ ]*int [a-zA-Z_0-9]*;" | egrep -v "^[ ]*int [a-zA-Z_0-9]*[ ]*=[ ]*[-0-9]+;" | egrep -v "^[ ]*final int [a-zA-Z_0-9]*[ ]*=[ ]*[-0-9]+;" | egrep -v "^[ ]*int [a-zA-Z_0-9]*[ ]*=[ ]*Integer.MIN_VALUE;" | egrep -v "^[ ]*int [a-zA-Z_0-9]*[ ]*=[ ]*Integer.MAX_VALUE;" \
 						        | egrep -v "^[ ]*short [a-zA-Z_0-9]*;" | egrep -v "^[ ]*short [a-zA-Z_0-9]*[ ]*=[ ]*[-0-9]+;" | egrep -v "^[ ]*final short [a-zA-Z_0-9]*[ ]*=[ ]*[-0-9]+;" \
 						        | egrep -v "^[ ]*long [a-zA-Z_0-9]*;" | egrep -v "^[ ]*long [a-zA-Z_0-9]*[ ]*=[ ]*[-0-9]+;" | egrep -v "^[ ]*final long [a-zA-Z_0-9]*[ ]*=[ ]*[-0-9]+;" \
 						        | egrep -v "^[ ]*byte [a-zA-Z_0-9]*;" | egrep -v "^[ ]*byte [a-zA-Z_0-9]*[ ]*=[ ]*[-0-9]+;" | egrep -v "^[ ]*final byte [a-zA-Z_0-9]*[ ]*=[ ]*[-0-9]+;" \
 						        | egrep -v "^[ ]*char [a-zA-Z_0-9]*;" | egrep -v "^[ ]*char [a-zA-Z_0-9]*[ ]*=[ ]*'[A-Za-z+-q_]';" | egrep -v "^[ ]*final char [a-zA-Z_0-9]*[ ]*=[ ]*'[A-Za-z+-q_]';" \
 						        | egrep -v "^[ ]*float [a-zA-Z_0-9]*;" | egrep -v "^[ ]*float [a-zA-Z_]*[ ]*=[ ]*[0-9]+;" \
-						        | egrep -v "^[ ]*double [a-zA-Z_]*;" | egrep -v "^[ ]*double [a-zA-Z_]*[ ]*=[ ]*[-0-9.]+;" | egrep -v "^[ ]*final double [a-zA-Z_0-9]*[ ]*=[ ]*[-0-9.]+;" | egrep -v "^[ ]*double [a-zA-Z_0-9]*[ ]*=[ ]*Double.MAX_VALUE+;" \
+						        | egrep -v "^[ ]*double [a-zA-Z_]*;" | egrep -v "^[ ]*double [a-zA-Z_]*[ ]*=[ ]*[-0-9.]+;" | egrep -v "^[ ]*final double [a-zA-Z_0-9]*[ ]*=[ ]*[-0-9.]+;" | egrep -v "^[ ]*double [a-zA-Z_0-9]*[ ]*=[ ]*Double.MIN_VALUE+;" | egrep -v "^[ ]*double [a-zA-Z_0-9]*[ ]*=[ ]*Double.MAX_VALUE+;" \
 						        | egrep -v "^[ ]*boolean [a-zA-Z_0-9]*;" | egrep -v "^[ ]*boolean [a-zA-Z_0-9]*[ ]*=[ ]*true;"| egrep -v "^[ ]*boolean [a-zA-Z_0-9]*[ ]*=[ ]*false;" | egrep -v "^[ ]*final boolean [a-zA-Z_0-9]*[ ]*=[ ]*true;"| egrep -v "^[ ]*final boolean [a-zA-Z_0-9]*[ ]*=[ ]*false;" \
 						        | egrep -v "^[ ]*String [a-zA-Z_0-9]*;" | egrep -v "^[ ]*String [a-zA-Z_0-9]*[ ]*=[ ]*\"[a-zA-Z_0-9 /\\,-;='\"!]*\";" | egrep -v "^[ ]*final String [a-zA-Z_0-9]*[ ]*=[ ]*\"[a-zA-Z_0-9 /\\,-;='\"!]*\";" \
 						        | egrep -v "^[ ]*[a-zA-Z_0-9]* [a-zA-Z_0-9]*[ ]*;" | egrep -v "^[ ]*[a-zA-Z_0-9]* [a-zA-Z_0-9]*[ ]*=[ ]*null;" \
