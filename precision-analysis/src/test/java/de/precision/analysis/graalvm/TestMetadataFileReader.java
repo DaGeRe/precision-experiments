c2b03030bd6c00620ece7dad6044d83c574515b1
David Georg Reichelt <davidgeorg_reichelt@dagere.de>
2023-09-11 15:26:48 +0200

    Fix TestMetadataFileReader

--- precision-analysis/src/test/java/de/precision/analysis/graalvm/TestMetadataFileReader.java
+++ precision-analysis/src/test/java/de/precision/analysis/graalvm/TestMetadataFileReader.java
@@ -17,11 +17,12 @@ public class TestMetadataFileReader {
       File folder = new File("src/test/resources/graalvm-example-data");
       Map<File, Date> fileDates = new MetadataFileReader(folder).getFileDates();
 
-      Date exampleDate1 = fileDates.get(new File("/home/reichelt/workspaces/dissworkspace/repos/graalvm/2022-01/measurement/30/39/74/7743930"));
-      Date date1 = MetadataFileReader.METADATA_TIME_FORMAT.parse("2022-01-13T02:31:26+00:00");
-      Assert.assertEquals(exampleDate1, date1);
+      // The files here are the pure data created by the phoenix extraction - they don't need to exist for test execution
+      Date exampleDate1 = fileDates.get(new File("/home/reichelt/nvme/workspaces/dissworkspace/phoenix/extracted/6/34/14/291/16/18/29449/65590"));
+      Date date1 = MetadataFileReader.METADATA_TIME_FORMAT.parse("2021-09-29T00:48:42+00:00");
+      Assert.assertEquals(date1, exampleDate1);
       
-      Date exampleDate2 = fileDates.get(new File("/home/reichelt/workspaces/dissworkspace/repos/graalvm/2022-01/measurement/30/33/71/7713330"));
+      Date exampleDate2 = fileDates.get(new File("/home/reichelt/nvme/workspaces/dissworkspace/phoenix/extracted/5/34/5/101/27/24/34197/71036"));
       Date date2 = MetadataFileReader.METADATA_TIME_FORMAT.parse("2021-12-29T17:19:37+00:00");
       Assert.assertEquals(exampleDate2, date2);
    }
