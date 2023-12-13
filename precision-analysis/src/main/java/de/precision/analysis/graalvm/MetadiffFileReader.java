1ed222b1aa06f253dba459ddf08b92e058edee1f
David Georg Reichelt <davidgeorg_reichelt@dagere.de>
2023-12-13 09:57:17 +0100

    Extract MetadiffFileReader

---
+++ precision-analysis/src/main/java/de/precision/analysis/graalvm/MetadiffFileReader.java
@@ -0,0 +1,92 @@
+package de.precision.analysis.graalvm;
+
+import java.io.File;
+import java.util.Date;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import de.dagere.peass.measurement.statistics.Relation;
+
+public class MetadiffFileReader {
+   
+   private static final Logger LOG = LogManager.getLogger(MetadiffFileReader.class);
+   
+   final int runOldIndex;
+   final int runNewIndex;
+   
+   private final int machineType, configuration, benchmark, versionOld, versionNew;
+   
+//   id = machine-type-configuration-benchmark-version_old
+   
+   final int pValueIndex ;
+   final int effectSizeIndex;
+   final int benchmarkIndex;
+   final int machineTypeIndex;
+   final int configurationIndex;
+   
+   private final MetadataFileReader metadataReader;
+   
+   public MetadiffFileReader(String headline, MetadataFileReader metadataReader) {
+      runOldIndex = GraalVMReadUtil.getColumnIndex(headline, "run_id_old");
+      runNewIndex = GraalVMReadUtil.getColumnIndex(headline, "run_id_new");
+      
+      machineType = GraalVMReadUtil.getColumnIndex(headline, "machineType");
+      configuration = GraalVMReadUtil.getColumnIndex(headline, "configuration");
+      benchmark = GraalVMReadUtil.getColumnIndex(headline, "benchmark");
+      versionOld = GraalVMReadUtil.getColumnIndex(headline, "version_old");
+      versionNew = GraalVMReadUtil.getColumnIndex(headline, "version_new");
+      
+//      id = machine-type-configuration-benchmark-version_old
+      
+      pValueIndex = GraalVMReadUtil.getColumnIndex(headline, "p_value");
+      effectSizeIndex = GraalVMReadUtil.getColumnIndex(headline, "size_effect");
+      benchmarkIndex = GraalVMReadUtil.getColumnIndex(headline, "benchmark");
+      machineTypeIndex = GraalVMReadUtil.getColumnIndex(headline, "machine_type");
+      configurationIndex = GraalVMReadUtil.getColumnIndex(headline, "configuration");
+      
+      this.metadataReader = metadataReader;
+   }
+   
+   public void handleLine(ComparisonCollection comparisons, String[] parts) {
+      String runOld = parts[runOldIndex];
+      String runNew = parts[runNewIndex];
+      double pValue = Double.parseDouble(parts[pValueIndex]);
+      double effectSize = Double.parseDouble(parts[effectSizeIndex]);
+      final int benchmark = Integer.parseInt(parts[benchmarkIndex]);
+      final int runsOld = Integer.parseInt(parts[benchmarkIndex]);
+      final int runsNew = Integer.parseInt(parts[benchmarkIndex]);
+      String benchmarkKey = parts[machineTypeIndex] + "-" + parts[configurationIndex] + "-" + parts[benchmarkIndex];
+
+      String comparisonId = runOld + "_" + runNew;
+
+      File folderOld = metadataReader.getFileById(runOld);
+      File folderNew = metadataReader.getFileById(runNew);
+      Date dateOld = metadataReader.getFileDates().get(folderOld);
+      Date dateNew = metadataReader.getFileDates().get(folderNew);
+      
+      if (folderOld != null && folderNew != null && 
+            folderOld.exists() && folderNew.exists()) {
+         Comparison comparison = new Comparison(comparisonId, folderOld, folderNew, dateOld, dateNew, benchmark, runsOld, runsNew);
+         
+         comparisons.addComparison(benchmarkKey, comparisonId, comparison);
+
+         comparison.setPValue(pValue);
+         if (pValue < 0.01) {
+            if (effectSize > 0) {
+               comparison.setRelation(Relation.LESS_THAN);
+            } else {
+               comparison.setRelation(Relation.GREATER_THAN);
+            }
+         } else {
+            comparison.setRelation(Relation.EQUAL);
+         }
+      } else {
+         if (comparisonId.startsWith("6-") && folderOld != null && folderNew != null) {
+            LOG.trace("Did not find " + comparisonId);
+            LOG.trace(folderOld.getAbsolutePath() + " " + folderOld.exists());
+            LOG.trace(folderNew.getAbsolutePath() + " " + folderNew.exists());
+         }
+      }
+   }
+}
